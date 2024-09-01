a39bd18f6baf3fe7a49f9bf2be8c2ccca7485048
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-09-02 00:43:40 +0300

    intial commit

---
+++ PeerShare.NetSync/PeerShare.NetSync/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PeerShare.NetSync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
