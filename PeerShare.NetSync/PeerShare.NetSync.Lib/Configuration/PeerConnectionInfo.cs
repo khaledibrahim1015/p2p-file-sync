d8fa706522ccdafa6a57822315a065592e73edaf
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-09-02 04:06:04 +0300

    impelement peerconnectionifo for incoming connection

---
+++ PeerShare.NetSync/PeerShare.NetSync.Lib/Configuration/PeerConnectionInfo.cs
@@ -0,0 +1,20 @@
+﻿using PeerShare.NetSync.Lib.Enums;
+
+namespace PeerShare.NetSync.Lib.Configuration;
+
+public class PeerConnectionInfo : IEquatable<PeerConnectionInfo>
+{
+    public string IpAddress { get; set; }
+    public int Port { get; set; }
+    public PeerRole Role { get; set; }
+
+    public bool Equals(PeerConnectionInfo? other)
+    {
+        return IpAddress == other.IpAddress && Port == other.Port && Role == other.Role;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(IpAddress, Port, Role);
+    }
+
+}
