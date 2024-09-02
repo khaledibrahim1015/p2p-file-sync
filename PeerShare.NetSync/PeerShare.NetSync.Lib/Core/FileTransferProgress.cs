a1c7c8ead1af6cdcd02037873ba3823dc2abbd40
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-09-02 13:24:49 +0300

    FileTransferProgress , FileTransferState

---
+++ PeerShare.NetSync/PeerShare.NetSync.Lib/Core/FileTransferProgress.cs
@@ -0,0 +1,12 @@
+﻿namespace PeerShare.NetSync.Lib.Core;
+
+public class FileTransferProgress
+{
+    public string FileName { get; set; }
+    public long BytesTransfered { get; set; }
+    public long TotalBytes { get; set; }
+
+    public double ProgressPercentage => (double)BytesTransfered / TotalBytes * 100;
+
+
+}
---
+++ PeerShare.NetSync/PeerShare.NetSync.Lib/Core/FileTransferState.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+namespace PeerShare.NetSync.Lib.Core;
+
+public class FileTransferState
+{
+    private const string StateDirectory = "C:\\Users\\ZALL-TECH\\Desktop\\P2PDistributedFileTransfer\\data\\temp";
+
+    public string FileName { get; set; }
+    public long TotalBytes { get; set; }
+    public long BytesTransferred { get; set; }
+    public string CheckSum { get; set; }
+
+    public static void SaveState(FileTransferState state)
+    {
+        if (!Directory.Exists(StateDirectory))
+        {
+            Directory.CreateDirectory(StateDirectory);
+        }
+
+        string filePath = Path.Combine(StateDirectory, $"{state.FileName}.json");
+        string json = JsonSerializer.Serialize(state);
+        File.WriteAllText(filePath, json);
+    }
+    public static void DeleteState(string fileName)
+    {
+        string filePath = Path.Combine(StateDirectory, $"{fileName}.json");
+        if (File.Exists(filePath))
+            File.Delete(filePath);
+    }
+
+
+
+
+
+
+
+}
