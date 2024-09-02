c7214147e5c2800c90ce0de411253ee83aae5ecc
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-09-02 13:25:31 +0300

    apply md5HashAlgorithm to secure intrupted fileTransfer

--- PeerShare.NetSync/PeerShare.NetSync.Lib/Helper/FileHelper.cs
+++ PeerShare.NetSync/PeerShare.NetSync.Lib/Helper/FileHelper.cs
@@ -1,9 +1,11 @@
 ﻿using PeerShare.NetSync.Lib.Core;
+using System.Security.Cryptography;
 
 namespace PeerShare.NetSync.Lib.Helper;
 
 public static class FileHelper
 {
+    private const int ChecksumChunkSize = 8192; // 8kb
 
     public static FileMetaData ParseFileMetaData(string metaData)
     {
@@ -16,5 +18,25 @@ public static class FileHelper
         };
     }
 
+    public static async Task<string> CalculateCheckSum(string filePath)
+    {
+        using HashAlgorithm md5Hash = MD5.Create();
+        using Stream fileStream = File.OpenRead(filePath);
 
+        byte[] buffer = new byte[ChecksumChunkSize];
+        int bytesRead;
+        do
+        {
+            bytesRead = await fileStream.ReadAsync(buffer, 0, buffer.Length);
+            if (bytesRead > 0)
+                md5Hash.TransformBlock(buffer, 0, bytesRead, null, 0);
+        } while (bytesRead != 0);
+
+        md5Hash.TransformFinalBlock(Array.Empty<byte>(), 0, 0);
+        return BitConverter.ToString(md5Hash.Hash).Replace("-", "").ToLowerInvariant();
+    }
+    public static void ReportProgress(FileTransferProgress progress)
+    {
+        Console.WriteLine($"Transferring {progress.FileName}: {progress.ProgressPercentage:F2}% complete");
+    }
 }
